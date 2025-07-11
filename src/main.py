5bf7699a73a87ca6e7254282785344de155a8ecb
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-07-11 15:50:40 +0200

    [feat] Add support for customizable file mode in logging configuration

--- src/main.py
+++ src/main.py
@@ -10,8 +10,9 @@ load_dotenv()
 
 log_level = os.environ.get("LOG_LEVEL", LogLevels.debug)
 log_filename = os.environ.get("LOG_FILENAME", None)
+log_filemode = os.environ.get("LOG_FILEMODE", "a")
 
-configure_logging(log_level, filename=log_filename)
+configure_logging(log_level, filename=log_filename, filemode=log_filemode)
 
 logging.info("DoDoes API is starting up!")
 logging.debug(f"Logging configuration: level={log_level}, filename={log_filename}")
