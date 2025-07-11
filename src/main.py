b20c15a64a3e98cc0915044019b35f5586535134
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-07-11 15:46:10 +0200

    [feat] Integrate logging configuration and enhance request logging in FastAPI

--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI
+import logging
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+
+from .logging import configure_logging, LogLevels
+
+load_dotenv()
+
+log_level = os.environ.get("LOG_LEVEL", LogLevels.debug)
+log_filename = os.environ.get("LOG_FILENAME", None)
+
+configure_logging(log_level, filename=log_filename)
+
+logging.info("DoDoes API is starting up!")
+logging.debug(f"Logging configuration: level={log_level}, filename={log_filename}")
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root(request: Request) -> dict:
+    logging.debug(f"Handling GET request for {request.url}")
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
+async def say_hello(name: str, request: Request) -> dict:
+    logging.debug(f"Handling GET request for {request.url} with name={name}")
     return {"message": f"Hello {name}"}
