5c4d3ae46f976695f7266f71310ca3728d5f10e0
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-07-11 15:06:08 +0200

    [feat] Enhance logging configuration to support file output and customizable file mode

--- src/logging.py
+++ src/logging.py
@@ -1,7 +1,6 @@
 import logging
 from enum import StrEnum
 
-
 LOG_FORMAT_DEBUG = "%(levelname)s:%(message)s:%(pathname)s:%(funcName)s:%(lineno)d"
 
 
@@ -12,16 +11,19 @@ class LogLevels(StrEnum):
     debug = "DEBUG"
 
 
-def configure_logging(log_level: str = LogLevels.error):
+def configure_logging(log_level: str = LogLevels.error, filename: str = None, filemode: str = "a") -> None:
     log_level = str(log_level).upper()
     log_levels = [level.value for level in LogLevels]
 
+    kwargs = {"level": log_level}
     if log_level not in log_levels:
-        logging.basicConfig(level=LogLevels.error)
-        return
+        kwargs["level"] = LogLevels.error
 
     if log_level == LogLevels.debug:
-        logging.basicConfig(level=log_level, format=LOG_FORMAT_DEBUG)
-        return
+        kwargs["format"] = LOG_FORMAT_DEBUG
 
-    logging.basicConfig(level=log_level)
+    if filename:
+        kwargs["filename"] = filename
+        kwargs["filemode"] = filemode
+
+    logging.basicConfig(**kwargs)
